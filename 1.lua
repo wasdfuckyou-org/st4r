4cb5eebc1a38466436743daf311e2cf0ce0b54b5
bubatz <71530056+cynhook@users.noreply.github.com>
2022-06-13 02:22:28 -0400

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,6 +1,6 @@
 	plr.CharacterAdded:Connect(function(Char)
 		Char:WaitForChild("Humanoid")
-		if plr.UserId == 457621896, then -- You may change to any player id.
+		if plr.UserId == 20930026, then -- You may change to any player id.
 			Char.Humanoid.DisplayName = "[💸]" .. plr.DisplayName -- Any emoji.
 		end
 	end)
